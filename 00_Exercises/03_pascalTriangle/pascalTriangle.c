474864b120441db147d65e67f869378837fa8901
Lukas Schmid <lukas.schmid@stud.hslu.ch>
2020-03-12 13:38:48 +0100

    meine Hausaufgabe 03_pascalTriangle

---
+++ 00_Exercises/03_pascalTriangle/pascalTriangle.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrintUsageMessage(void);
+void PrintPascalTriangle( int pascalLimit );
+
+const int lowerLimit = 1;
+const int upperLimit = 20;
+
+int main( int argc, char* argv[] )
+{
+	
+	int inputValue = 0;
+	if( argc != 2 )
+	{
+		PrintUsageMessage();
+		return 0;
+	} 
+	else
+	{
+		inputValue = atoi(argv[1]);
+		if( inputValue >= lowerLimit && inputValue <= upperLimit )
+		{
+			PrintPascalTriangle( inputValue );
+		}
+		else
+		{
+			// no valid inputcc
+			PrintUsageMessage();
+			return 0;
+		}	
+	}
+	return 0;
+}
+
+
+void PrintUsageMessage(void)
+{
+	printf("Usage: pascalTriangle wholeNumber\n\twholeNumber: integer on the interval [%d,%d]\n", lowerLimit, upperLimit);
+}
+
+
+void PrintPascalTriangle( int pascalLimit )
+{
+	int coef = 1, space;
+	for(int i = 0; i < pascalLimit; i++)
+	{
+		for (space=1; space <= pascalLimit-i; space++)
+			printf("  ");
+		for(int j = 0; j <= i; j++ )
+		{
+			if (j==0 || i==0)
+				coef = 1;
+			else
+				coef=coef*(i-j+1)/j;
+			printf("%4d", coef);
+		}
+		printf("\n");
+	}
+}
---
+++ 01_InClass/010_operators/operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+
+{
+	short largeShort = 32760;
+	largeShort = largeShort + 12345;
+	printf("largeShort: %d\n", largeShort);
+
+	int numerator = 19, denominator = 5, result;
+	result = numerator/denominator;
+	printf("result: %d\n", result);
+
+	float e = 2.18281828f;
+	e++; // -> print e
+	e--;
+	printf("e: %f\n", ++e);
+	printf("e: %f\n", e);
+	printf("e: %f\n", --e);
+	return 0;
+}
